c73d3bd66e8374b8dc1003ae113c7416d9a9b2d4
Ysj <brownstory82@gmail.com>
2026-01-14 20:49:25 +0900

    api study

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -8,8 +8,13 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args); // HelloSpringApplication 어노테이션 실행
+//		int a = add(1,2);
+//		System.out.printf("%d", a);
 	}
 
+//	static int add(int a, int b) {
+//		return a + b;
+//	}
 }
 /*
 git hub test hello world
