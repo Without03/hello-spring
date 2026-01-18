b454b661983b6d24fe518b1061215f99574f2c04
Ysj <brownstory82@gmail.com>
2026-01-19 04:26:15 +0900

    회원 서비스 테스트

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,18 +2,21 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
-    register
-     */
+        register
+         */
     public Long join(Member member) {
         // 동명이인 가입 불가
 //        Optional<Member> result = memberRespository.findByName(member.getName());
